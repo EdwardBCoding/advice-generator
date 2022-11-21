8769df28ce95f5df34314a277bbd626d002a7e5d
Edward <edward.b.workin@gmail.com>
2022-11-21 00:26:34 -0500

    more promise and fetch learning

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-fetch('	https://api.adviceslip.com/advice/2')
-    .then(res => res.json())
-    .then(data => advc = data.slip.advice)
+const adviceText = document.querySelector('.advice-text')
 
-    .then(console.log("advice is "+ advc))
+fetch('https://api.adviceslip.com/advice')
+    // .then(res => {
+    // if(res.ok){
+    //     console.log('yay')
+    // } else{
+    //     console.log('weee gotttaaa problem')
+    // }})
+    // .then(data => console.log(data))
+    .then(res => res.json())
+    .then(data => {
+        let adv = data.slip.advice
+        adviceText.innerText = adv
+    })
+    // .then(data => console.log(data.slip.advice))
+
