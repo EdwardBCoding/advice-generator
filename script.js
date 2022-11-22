df5ecdba33a34a9398ee5aa8c0ec0041da7eedeb
Edward <edward.b.workin@gmail.com>
2022-11-22 18:39:33 -0500

    styles and js almost done

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const adviceText = document.querySelector('.advice-text')
+//! Selectors
 
-fetch('https://api.adviceslip.com/advice')
+const adviceText = document.querySelector('.advice-text')
+const adviceIdText = document.querySelector('.advice-id')
+const body = document.querySelector('body')
+const rerollButton = document.querySelector('.reroll-button')
+
+//! Listeners
+
+document.addEventListener('DOMContentLoaded', fetchAdvice)
+rerollButton.addEventListener('click', fetchAdvice)
+
+//! Functions
+
+function test(){
+    console.log('weener')
+}
+
+function fetchAdvice(){
+    fetch('https://api.adviceslip.com/advice')
     // .then(res => {
     // if(res.ok){
     //     console.log('yay')
@@ -9,9 +26,14 @@ fetch('https://api.adviceslip.com/advice')
     // }})
     // .then(data => console.log(data))
     .then(res => res.json())
-    .then(data => {
-        let adv = data.slip.advice
-        adviceText.innerText = adv
-    })
+    .then(data => {writeAdvice(data)})
     // .then(data => console.log(data.slip.advice))
+}
 
+function writeAdvice(data){
+    // console.log(data)
+    let adv = data.slip.advice
+    adviceText.innerText = adv
+    let advId = data.slip.id
+    adviceIdText.innerText = advId
+}
